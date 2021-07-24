42a072041165231ce8ee9c0b6a18d3b27dfb1501
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-07-23 23:34:09 -0300

    config flake8 e CI

--- ongProject/urls.py
+++ ongProject/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
+from django.conf import settings # noqa
 
 urlpatterns = [
     path('admin/', admin.site.urls),
